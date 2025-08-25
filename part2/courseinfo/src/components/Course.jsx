e406c19840c5b6edba83a7226db499b9459cf606
Tamar Revazishvili <tamar.d154@gmail.com>
2025-08-25 18:04:41 +0300

    2.1: Course information step 6

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,25 @@
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+const Content = ({ parts }) => {
+  return parts.map((part) => <Part key={part.id} part={part} />);
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+export default Course;
