c5f3b4d6a0fb1e12423d4a8d332d774eeb7fdf8f
Tamar Revazishvili <tamar.d154@gmail.com>
2025-08-25 13:00:08 +0300

    1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
 
   /**
    * the feedback values are: good 1, neutral 0, bad -1
    */
-  const total = good + neutral + bad;
-
   const calculateAverage = () => {
     return (good * 1 + bad * -1) / total;
   };
@@ -19,26 +15,35 @@ const App = () => {
   };
 
   return (
+    <p>
+      good {good}
+      <br />
+      neutral {neutral}
+      <br />
+      bad {bad}
+      <br />
+      all {total}
+      <br />
+      average {calculateAverage()}
+      <br />
+      positive {calculatePercent()} %
+      <br />
+    </p>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
     <div>
       <h2>give feedback</h2>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-
-      <p>
-        good {good}
-        <br />
-        neutral {neutral}
-        <br />
-        bad {bad}
-        <br />
-        all {total}
-        <br />
-        average {calculateAverage()}
-        <br />
-        positive {calculatePercent()} %
-        <br />
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
