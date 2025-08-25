6e58f8581851a17f240c69ca2adefdfad33931f0
Tamar Revazishvili <tamar.d154@gmail.com>
2025-08-25 12:50:24 +0300

    1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  /**
+   * the feedback values are: good 1, neutral 0, bad -1
+   */
+  const total = good + neutral + bad;
+
+  const calculateAverage = () => {
+    return (good * 1 + bad * -1) / total;
+  };
+
+  const calculatePercent = () => {
+    return (good / total) * 100;
+  };
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -12,9 +25,20 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>
+        good {good}
+        <br />
+        neutral {neutral}
+        <br />
+        bad {bad}
+        <br />
+        all {total}
+        <br />
+        average {calculateAverage()}
+        <br />
+        positive {calculatePercent()} %
+        <br />
+      </p>
     </div>
   );
 };
