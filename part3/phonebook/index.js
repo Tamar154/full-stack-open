655444d3f575b88cb367d9dd50f0e6c8acb0622e
Tamar Revazishvili <tamar.d154@gmail.com>
2025-09-01 09:33:17 +0300

    3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", (request) =>
+  request.method === "POST" ? JSON.stringify(request.body) : ""
+);
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
